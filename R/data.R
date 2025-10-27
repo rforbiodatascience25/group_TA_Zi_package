334efa1dcfe7eb658512aa40dea449062e919a2a
ztizh <ztizh@dtu.dk>
2025-10-27 17:46:47 +0100

    Added codon_table

---
+++ R/data.R
@@ -0,0 +1,8 @@
+#' Standard RNA Codon Table
+#'
+#' A named character vector mapping RNA codons (triplets) to their
+#' corresponding amino acid one-letter codes.
+#'
+#' @format A named character vector with 64 elements.
+#' @source \url{https://www.ncbi.nlm.nih.gov/Taxonomy/Utils/wprintgc.cgi?chapter=tgencodes#SG1}
+"codon_table"
